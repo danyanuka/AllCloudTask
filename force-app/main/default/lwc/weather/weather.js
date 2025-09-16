272fd73d90c72420c74c78550900cbda0cad0270
danyanuka <danyanuka@gmail.com>
2025-09-17 00:18:36 +0300

    Basic UI

--- force-app/main/default/lwc/weather/weather.js
+++ force-app/main/default/lwc/weather/weather.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 
-export default class Weather extends LightningElement {}
+
+export default class Weather extends LightningElement {
+  @api recordId
+  shippingAddressLabel = ''
+
+  @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+  wiredObjectInfo({ data }) {
+      if (data) {
+          this.shippingAddressLabel = data.fields.ShippingAddress.label;
+      }
+  }
+
+  @wire(getRecord, { recordId: '$recordId', fields: [] })
+  account;
+  
+
+  get cardTile(){
+    return `Weather In - ${this.shippingAddressLabel}`
+  }
+
+
+}
