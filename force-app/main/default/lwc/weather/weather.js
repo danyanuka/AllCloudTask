8c9f8fd5d6a0051ec342d801c84f04325efca859
danyanuka <danyanuka@gmail.com>
2025-09-18 01:12:00 +0300

    Backend service created / JS- ShippingAdress from curr acc

--- force-app/main/default/lwc/weather/weather.js
+++ force-app/main/default/lwc/weather/weather.js
@@ -2,21 +2,35 @@ import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import SHIPPING_ADDRESS from '@salesforce/schema/Account.ShippingAddress';
+import SHIPPING_CITY from '@salesforce/schema/Account.ShippingCity';
 
 
 export default class Weather extends LightningElement {
   @api recordId
   shippingAddressLabel = ''
+  shippingAddress;
 
   @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
   wiredObjectInfo({ data }) {
       if (data) {
-          this.shippingAddressLabel = data.fields.ShippingAddress.label;
+          this.shippingAddressLabel = data.fields.ShippingAddress.label
       }
   }
 
-  @wire(getRecord, { recordId: '$recordId', fields: [] })
-  account;
+  @wire(getRecord, { recordId: '$recordId', fields: [SHIPPING_CITY] })
+    wiredAccount({ error, data }) {
+        if (data) {
+            this.shippingAddress = data.fields.ShippingCity.value
+            console.log('shippingAddress' , this.shippingAddress);
+            
+        } else if (error) {
+            this.shippingAddress = undefined
+        }
+    }
+
+
+
   
 
   get cardTile(){
