31013e9903cf6b89f75991113f882eeff51fd5de
danyanuka <danyanuka@gmail.com>
2025-09-19 04:18:25 +0300

    Design html/css  / Dynamic title / Loading state /

--- force-app/main/default/lwc/weather/weather.js
+++ force-app/main/default/lwc/weather/weather.js
@@ -1,44 +1,72 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import ACCOUNT_OBJECT from '@salesforce/schema/Account';
-import SHIPPING_ADDRESS from '@salesforce/schema/Account.ShippingAddress';
-import SHIPPING_CITY from '@salesforce/schema/Account.ShippingCity';
+import { LightningElement, api, wire , track} from 'lwc'
+import { getRecord } from 'lightning/uiRecordApi'
+import { getObjectInfo } from 'lightning/uiObjectInfoApi'
+//Schemas
+import ACCOUNT_OBJECT from '@salesforce/schema/Account'
+import SHIPPING_CITY from '@salesforce/schema/Account.ShippingCity'
+import NAME from '@salesforce/schema/Account.Name'
+//Apex
+import getTomorrowWeatherByCity from '@salesforce/apex/AdressWeatherService.getTomorrowWeatherByCity'
 
 
 export default class Weather extends LightningElement {
   @api recordId
+
+  weatherInfo
+  
   shippingAddressLabel = ''
-  shippingAddress;
+  shippingCity
+  accountName
+  isLoading = false
 
   @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
-  wiredObjectInfo({ data }) {
-      if (data) {
-          this.shippingAddressLabel = data.fields.ShippingAddress.label
-      }
+    accountObjectInfo({ data }) {
+        if (data) {
+            this.shippingAddressLabel = data.fields.ShippingAddress.label
+            this.billingAddressLabel = data.fields.BillingAddress.label
+        }
   }
 
-  @wire(getRecord, { recordId: '$recordId', fields: [SHIPPING_CITY] })
+  //API Req will execute On Initial load and whenever fields[] change. 
+  @wire(getRecord, { recordId: '$recordId', fields: [NAME,SHIPPING_CITY] })
     wiredAccount({ error, data }) {
         if (data) {
-            this.shippingAddress = data.fields.ShippingCity.value
-            console.log('shippingAddress' , this.shippingAddress)
-            
+            const newCity = data.fields.ShippingCity.value;
+            if (newCity !== this.shippingCity) { //Avoids redundant API calls
+                this.shippingCity = newCity;
+                this.accountName = data.fields.Name.value || `Account's`;
+                this.callWeatherService();
+            }
         } else if (error) {
-            this.shippingAddress = undefined
+            this.weatherInfo = undefined;
+            this.shippingCity = undefined
+            console.error('Error fetching account record', error);
         }
     }
 
-
-
-  
-
-  get cardTile(){
-    return `Weather In - ${this.shippingAddressLabel}`
+  async callWeatherService() {
+      try {
+        console.log('RAN!');
+        
+          this.weatherInfo = await getTomorrowWeatherByCity({ city: this.shippingCity })
+      } catch (error) {
+          console.error('Error fetching weather:', error)
+      } finally{
+          this.isLoading = false
+      }
   }
 
-  get minMaxTempRange(){
-    return `${Math.round(minTempC)}° - ${Math.round(maxTempC)}°`
+
+  get cardTile(){
+      return `Tomorrow's weather in ${this.accountName}'s ${this.shippingAddressLabel}`
+  }
+
+  get minTempRound(){
+    return `${Math.round(this.weatherInfo.minTempC)}°`
+  }
+
+  get maxTempRound(){
+    return `${Math.round(this.weatherInfo.maxTempC)}°`
   }
 
 
